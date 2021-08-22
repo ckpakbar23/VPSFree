0603d00482ce2d6f9a613f4a5d0ab209c0737455
akbarfx23 <6412777+akbarfx23@users.noreply.github.com>
2021-08-23 02:27:25 +0700

    Add files via upload

--- ngrok.py
+++ ngrok.py
@@ -4,11 +4,10 @@ filename = "/root/.ngrok2/ngrok.yml"
 if os.path.exists(filename):
     fp = open(filename, "r")
     content = fp.read()
-    data = content.split(": ")
-    authtoken = data[1]
+    name, authtoken = content.strip("\r\n").split(": ")
     ngrok.set_auth_token(authtoken)
     conf.get_default().auth_token = authtoken
     connection_string = ngrok.connect(22, "tcp").public_url
     ssh_url, port = connection_string.strip("tcp://").split(":")
-    print(f" **ssh root@{ssh_url} -p{port}**")
+    print(f" **    ssh root@{ssh_url} -p{port}    **")
     fp.close()
