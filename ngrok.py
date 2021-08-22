5a70b69462311f0daa37f9ba66bf92d56621acb5
akbarfx23 <6412777+akbarfx23@users.noreply.github.com>
2021-08-22 23:55:51 +0700

    Update ngrok.py

--- ngrok.py
+++ ngrok.py
@@ -1,8 +1,5 @@
 
 from pyngrok import ngrok, conf
-authtoken = "1x3O8vLLvvKVmwgs8OML9oAA8CO_dh1R6S1ApztNGFZX5ppT"
-ngrok.set_auth_token(authtoken)
-conf.get_default().auth_token = authtoken
 connection_string = ngrok.connect(22, "tcp").public_url
 ssh_url, port = connection_string.strip("tcp://").split(":")
 print(f" **ssh root@{ssh_url} -p{port}**")
