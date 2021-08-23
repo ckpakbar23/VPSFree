83e077fc6219ed3901dd1ec3c0ec1979485c9a60
akbarfx23 <6412777+akbarfx23@users.noreply.github.com>
2021-08-23 12:43:22 +0700

    Add files via upload

--- ngrok.py
+++ ngrok.py
@@ -1,13 +1,8 @@
-import os
-from pyngrok import ngrok, conf
-filename = "/root/.ngrok2/ngrok.yml"
-if os.path.exists(filename):
-    fp = open(filename, "r")
-    content = fp.read()
-    name, authtoken = content.strip("\r\n").split(": ")
-    ngrok.set_auth_token(authtoken)
-    conf.get_default().auth_token = authtoken
-    connection_string = ngrok.connect(22, "tcp").public_url
-    ssh_url, port = connection_string.strip("tcp://").split(":")
-    print(f" **    ssh root@{ssh_url} -p{port}    **")
-    fp.close()
+import requests
+import json
+response = requests.get("http://localhost:4040/api/tunnels")
+data = response.json()
+connection_string = data["tunnels"][0]["public_url"]
+ssh_url, port = connection_string.strip("tcp://").split(":")
+print("######[VPS DATA INFO LOGIN]######")
+print(f"HOST\t\t: {ssh_url}\nPORT\t\t: {port}\nUSERNAME\t: root\nPASSWORD\t: VPSFree1324\nCOMMAND\t\t: ssh root@{ssh_url} -p{port}")
